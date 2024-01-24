b2a473ffcce001d398a621db43f096be037dccf8
jofer10 <jorge_galindo614@hotmail.com>
2024-01-24 09:08:30 -0500

    Modificacion v1

--- src/app/componentes/cabecero/cabecero.component.ts
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cabecero',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './cabecero.component.html',
   styleUrl: './cabecero.component.css'
 })
 export class CabeceroComponent {
-
+  
+  constructor(private router:Router){}
+  
+  ir(){
+  this.router.navigate(["registrarse"])
+  }
 }
